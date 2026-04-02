e471cd081e1f991e640d9e39e160d0d195af0ee0
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-04-02 20:51:16 +0700

    feat: make validation, services, controller, and add to routes/api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AttendanceController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    // User / Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/users/current', [AuthController::class, 'current']);
+
+    // Attendances
+    Route::get('/attendances', [AttendanceController::class, 'index']);
+    Route::post('/attendances', [AttendanceController::class, 'store']);
+    Route::get('/attendances/today', [AttendanceController::class, 'today']);
+});
